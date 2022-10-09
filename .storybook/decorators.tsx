a8b155b15ec3820389c1d2e2676326195bc91048
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2022-10-09 09:38:25 -0500

    added global decorator for themes

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -1,14 +1,18 @@
 import { DecoratorFn } from '@storybook/react'
 import { GlobalStyle } from '../src/styles/GlobalStyle'
 import { ThemeProvider } from 'styled-components'
-import { lightTheme } from '../src/styles/theme'
+import { lightTheme, darkTheme } from '../src/styles/theme'
 
-const withTheme: DecoratorFn = (StoryFn) => (
-  <ThemeProvider theme={lightTheme}>
-    <GlobalStyle />
-    <StoryFn />
-  </ThemeProvider>
-)
+const withTheme: DecoratorFn = (StoryFn, context) => {
+  const theme = context.globals.theme
+  const storyTheme = theme === 'dark' ? darkTheme : lightTheme
+  return (
+    <ThemeProvider theme = { storyTheme } >
+      <GlobalStyle />
+      <StoryFn />
+    </ThemeProvider>
+  )
+}
 
 export const globalDecorators = [
   withTheme
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -33,3 +33,16 @@ export const parameters = {
 }
 
 export const decorators = globalDecorators
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      items: ['light', 'dark'],
+      showName: true,
+    },
+  },
+};
