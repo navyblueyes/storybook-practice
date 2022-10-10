c60e5e72c362944904017d1f453deb72d6212c77
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2022-10-09 21:06:08 -0500

    Restaurant Card complete

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -2,9 +2,10 @@ import { DecoratorFn } from '@storybook/react'
 import { GlobalStyle } from '../src/styles/GlobalStyle'
 import { ThemeProvider } from 'styled-components'
 import { lightTheme, darkTheme } from '../src/styles/theme'
+import { withDesign } from 'storybook-addon-designs'
 
 const withTheme: DecoratorFn = (StoryFn, context) => {
-  const theme = context.globals.theme
+  const theme = context.parameters.theme || context.globals.theme
   const storyTheme = theme === 'dark' ? darkTheme : lightTheme
   return (
     <ThemeProvider theme = { storyTheme } >
@@ -15,5 +16,6 @@ const withTheme: DecoratorFn = (StoryFn, context) => {
 }
 
 export const globalDecorators = [
-  withTheme
+  withTheme,
+  withDesign
 ]
--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -8,8 +8,19 @@ export default {
   args: {
     text: 'Comfort food',
   },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?node-id=1906%3A3469',
+    },
+  },
 } as ComponentMeta<typeof Badge>
 
 const Template: ComponentStory<typeof Badge> = (args) => <Badge {...args} />
 
 export const Default = Template.bind({})
+export const DarkTheme = Template.bind({})
+
+DarkTheme.parameters = {
+  theme: 'dark',
+}
